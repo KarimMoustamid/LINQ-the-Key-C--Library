407d78a990ae5850f3a2270a3449b0a68ef16a99
KarimMoustamid <karim.moustamid@agilethought.com>
2023-03-26 12:10:40 -0600

    Lambda Expression

--- IntroToLinq/Program.cs
+++ IntroToLinq/Program.cs
@@ -1,35 +1,45 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
 
-var wordsNoUpperCase = new List<string> {"hello", "world", "this", "is", "a", "list", "of", "words"};
-Console.WriteLine(IsAnyWordUpperCase(wordsNoUpperCase));
 
-var wordsWithUpperCase = new List<string>
-    {"hello", "world", "this", "is", "a", "list", "of", "words", "with", "UPPERCASE"};
-Console.WriteLine(IsAnyWordUpperCase(wordsWithUpperCase));
 
-static bool IsAnyWordUpperCase(IEnumerable<string> words)
+var numbers = new[] {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+var isAnyLargerThan100 = IsAnyLargerThan100(numbers , number => number > 100);
+var isAnyEven = IsAnyEven(numbers , number => number % 2 == 0);
+
+
+Console.WriteLine($"Is any > 100 ? : {isAnyLargerThan100}");
+Console.WriteLine($"Is any Even ? : {isAnyEven}");
+
+
+static bool IsAnyLargerThan100(IEnumerable<int> numbers , Func<int , bool> predicate)
 {
-    foreach (var word in words)
+    foreach (var number in numbers)
     {
-        var areAllUpper = true;
-        foreach (var letter in word)
-        {
-            if (char.IsLower(letter))
-            {
-                areAllUpper = false;
-            }
-        }
-
-        if (areAllUpper)
-        {
-            return true;
-        }
+        return predicate(number);
     }
 
     return false;
 }
 
-static bool IsAnyWordUpperCase_Linq(IEnumerable<string> words)
+static bool IsAnyEven(IEnumerable<int> numbers , Func<int, bool> predicate)
 {
-    return words.Any(word => word.All(letter => char.IsUpper(letter)));
+    foreach (var number in numbers)
+    {
+        return predicate(number);
+    }
+
+    return false;
 }
+
+// static bool IsLargerThan100(int number)
+// {
+//     return number > 100;
+// }
+
+// static bool IsEven(int number)
+// {
+//     return number % 2 == 0;
+// }
+
+
