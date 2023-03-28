bd0dd468b46865e419dd5ddc711d6543e90285f9
KarimMoustamid <karim.moustamid@agilethought.com>
2023-03-28 06:52:42 -0600

    Deferred execution in LINQ

--- IntroToLinq/Program.cs
+++ IntroToLinq/Program.cs
@@ -1,12 +1,68 @@
-﻿var numbers = new List<int> {1 , 2, 3, 4, 5, 6, 7, 8, 9};
+﻿
+var words = new List<string> { "the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog" };
 
-var numbersWith10 = numbers.Append(10);
+var shortWords = words.Where(w => w.Length <= 4).ToList();
+
+Console.WriteLine("First iteration The short words are:");
+foreach (var word in shortWords)
+{
+    Console.WriteLine(word);
+}
+
+words.Add("rice");
+
+Console.WriteLine("Second iteration The short words are:");
+foreach(var word in shortWords)
+{
+    Console.WriteLine(word);
+}
 
 
-// Console.WriteLine("Numbers: " + string.Join(", ", numbers)); 
-// Console.WriteLine("Numbers: " + string.Join(", ", numbersWith10)); 
+// var people = new List<Person>
+// {
+//     new () {Name = "John", country = "USA"},
+//     new () {Name = "Jane", country = "USA"},
+//     new () {Name = "Joe", country = "UK"}
+// };
+
+// var allAmericans = people.Where(p => p.country == "USA");
+// var notAllAmericans = allAmericans.Take(100);
+//
+// public class Person
+// {
+//     public string Name { get; set; }
+//     public string country { get; set; }
+// }
+//
+
+var animals = new List<string> { "dog", "cat", "bird", "fish", "dog", "cat", "dog" };
+
+var animalsWithD = animals.Where(a =>
+{
+    Console.WriteLine("Checking animal: " + a);
+    return a.StartsWith("d");
+});
+
+foreach (var animal in animalsWithD)
+{
+    Console.WriteLine(animal);
+}
 
 
-var oddNumbers = numbers.Where(n => n % 2 == 1).OrderBy(n => n);
-// var orderOddNumbers = oddNumbers.OrderBy(n => n);
-Console.WriteLine("IsOrderOddNumbers: " + string.Join(", ", oddNumbers)); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
